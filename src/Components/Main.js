e7a4d5ea7b4e3436603acf58f259824e230326d4
Max Faupel <max.faupel@gmx.de>
2022-01-06 18:32:04 +0100

    simulate fetch from database

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,23 +6,11 @@ class Main extends Component {
     constructor() {
         super()
         this.state = {
-            posts: [{
-                id: "0",
-                description: "beautiful landscape",
-                imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
-                    "3919321_1443393332_n.jpg"
-            }, {
-                id: "1",
-                description: "Aliens???",
-                imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg"
-            }, {
-                id: "2",
-                description: "On a vacation!",
-                imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-            }]
+            posts: []
         }
 
         this.removePhoto = this.removePhoto.bind(this)
+        console.log("constructor")
     }
 
     removePhoto(postRemoved) {
@@ -31,13 +19,44 @@ class Main extends Component {
             posts: state.posts.filter(post => post !== postRemoved)
         }))
     }
+
+    componentDidMount() {
+        const data = SimulateFetchFromDatabase()
+        this.setState({
+            posts: data
+        })
+        console.log("componentDidMount")
+    }
+
+    componentDidUpdate() {
+        alert("re-render")
+    }
+
     render() {
+        console.log("render")
         return (
             <div>
                 <Title text={'photowall'}></Title>
                 <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto}></PhotoWall>
             </div>)
     }
+}
+
+function SimulateFetchFromDatabase() {
+    return [{
+        id: "0",
+        description: "beautiful landscape",
+        imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
+            "3919321_1443393332_n.jpg"
+    }, {
+        id: "1",
+        description: "Aliens???",
+        imageLink: "https://s3.india.com/wp-content/uploads/2017/12/rocket.jpg"
+    }, {
+        id: "2",
+        description: "On a vacation!",
+        imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
+    }]
 }
 
 export default Main
