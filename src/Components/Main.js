36c14e198f0a61c756ed3082c565c31b30f597da
Max Faupel <max.faupel@gmx.de>
2022-01-09 22:18:54 +0100

    fix warnings

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,15 +1,10 @@
 import { Component } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AddPhoto from "./AddPhoto";
-import Photo from "./Photo";
 import PhotoWall from "./PhotoWall";
 import Title from "./Title";
 
 class Main extends Component {
-    constructor() {
-        super()
-    }
-
     render() {
         console.log(this.props)
         return (
